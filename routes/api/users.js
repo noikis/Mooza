4671386d03fa197e697e54890812c18f97446c68
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-05-03 03:33:27 +0700

    user validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator/check');
+
 const router = express.Router();
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register route
 // @access  Public
-router.get('/', (req, res) => res.send('Users route'));
+router.post(
+  '/',
+  [
+    // Validation
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('Users route');
+  }
+);
 
 module.exports = router;
