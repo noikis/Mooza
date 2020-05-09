5c2c927ed1d9974c0f8ea65790b6563c5d7dff0e
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-05-10 00:27:37 +0700

    setup react

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+import './App.css';
+import { Landing } from './components/layout/Landing';
+import { Navbar } from './components/layout/Navbar';
+
+const App = () => (
+  <Fragment>
+    <Navbar />
+    <Landing />
+  </Fragment>
+);
+
+export default App;
