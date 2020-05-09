2a734c87d9eb4a4ad62491aeea053e24680a0709
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-05-10 01:14:13 +0700

    auth/Register component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import { Landing } from './components/layout/Landing';
 import { Navbar } from './components/layout/Navbar';
+import { Register } from './components/auth/Register';
+import { Login } from './components/auth/Login';
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
