f228cf32a96a888bc1fea42075086617de814c99
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-05-12 02:45:45 +0700

    setup redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
+// React
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+// CSS
 import './App.css';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+// Components
 import { Landing } from './components/layout/Landing';
 import { Navbar } from './components/layout/Navbar';
-import { Register } from './components/auth/Register';
-import { Login } from './components/auth/Login';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Route exact path='/register' component={Register} />
-        <Route exact path='/login' component={Login} />
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,10 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-export const Register = () => {
+import { setAlert } from '../../actions/alert';
+
+const Register = (props) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -14,12 +18,32 @@ export const Register = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Password do no match');
+      props.setAlert('Password do not match', 'danger');
     } else {
-      console.log(formData);
+      console.log('SUCCESS');
+      // const newUser = {
+      //   name,
+      //   email,
+      //   password,
+      // };
+
+      // console.log(newUser);
+
+      // try {
+      //   const config = {
+      //     headers: {
+      //       'Content-Type': 'application/json',
+      //     },
+      //   };
+      //   const body = JSON.stringify(newUser);
+      //   const res = await axios.post('api/users/', body, config);
+      //   console.log(res.data);
+      // } catch (err) {
+      //   console.error(err.response.data);
+      // }
     }
   };
 
@@ -76,8 +100,10 @@ export const Register = () => {
         <input type='submit' className='btn btn-primary' value='Register' />
       </form>
       <p className='my-1'>
-        Already have an account? <a href='login.html'>Sign In</a>
+        Already have an account? <Link to='/login'>Sign In</Link>
       </p>
     </Fragment>
   );
 };
+
+export default connect(null, { setAlert })(Register);
