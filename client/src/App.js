df6eda9281f2ccd7624c12b9ff7e9429cf384832
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-08-08 06:51:31 +0700

    add education

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routes/PrivateRoute';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
 
 if (localStorage.token) {
   setToken(localStorage.token);
@@ -47,6 +48,11 @@ const App = () => {
                 exact
                 path='/edit-profile'
                 component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperience}
               />
             </Switch>
           </section>
